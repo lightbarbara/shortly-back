f2437d8d30bdd0ca53a164f51fe7001b0a1c9224
Barbara Luz <barbaraluz@eq.ufrj.br>
2022-12-21 00:22:51 -0300

    fix: user routes

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,10 +1,16 @@
+import bcrypt from 'bcrypt'
+import connection from '../database/db.js'
+import { v4 as uuid } from 'uuid'
+
 export async function signUp(req, res) {
 
     const { name, email, password } = req.locals
 
+    const encryptedPassword = bcrypt.hashSync(password, 15)
+
     try {
 
-        await connection.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, password])
+        await connection.query(`INSERT INTO users (name, email, password) VALUES ($1, $2, $3);`, [name, email, encryptedPassword])
 
         res.sendStatus(201)
 
@@ -16,6 +22,18 @@ export async function signUp(req, res) {
 
 export async function signIn(req, res) {
 
-    
+    const { id } = res.locals
+
+    try {
+
+        const token = uuid()
+
+        await connection.query(`INSERT INTO sessions ("userId", token) VALUES ($1, $2)`, [id, token])
+
+        res.status(200).send({ token })
+
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 
 }
